a42fa9e80ddb936c24b8b57daa2e2a6d24ff8e55
Eric Jiang <ericjiang107@gmail.com>
2021-07-15 02:00:28 -0400

    Python Assignment 3

---
+++ area.py
@@ -0,0 +1,9 @@
+def HouseArea(a, b):
+    print(f'The area of your house is: {a*b}')
+    
+    
+    
+    
+def PieC(r):
+    print(f'The circumfrence of the circle is: {2 * r * 3.1415926535}')
+          
